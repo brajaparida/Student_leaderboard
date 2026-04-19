9b18eec1cb425cfe1947b7ee01044dd30ed95473
brajaparida <46571829+brajaparida@users.noreply.github.com>
2026-04-19 18:35:23 +0530

    Add comment for changes section in leaderboard file

--- chessensei_leaderboard.py
+++ chessensei_leaderboard.py
@@ -4,7 +4,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 import time
 import json
-
+#changes
 # ─────────────────────────────────────────────
 # PAGE CONFIG
 # ─────────────────────────────────────────────
